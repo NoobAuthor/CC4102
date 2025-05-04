d2af649dff67effd2d5f641be671086bce87c4bb
CincoVicenteCoronado <vicentemoriarti@gmail.com>
2025-05-04 13:59:04 -0400

    refactor: Rehacer funciones auxiliares en nueva branch
    - Se reimplementaron las funciones

--- T1/src/external_mergesort.cpp
+++ T1/src/external_mergesort.cpp
@@ -12,6 +12,11 @@ const size_t B_LIMIT = BLOQUE / sizeof(int);
 
 using namespace std;
 
+void refill_buffer(ifstream& in, vector<int>& buffer, size_t& count) {
+    in.read(reinterpret_cast<char*>(buffer.data()), BLOQUE);
+    count = in.gcount() / sizeof(int);
+}
+
 void phase1_split_and_sort(const string& input_file, vector<string>& temp_files) {
     ifstream infile(input_file, ios::binary);
     if (!infile) {
@@ -24,17 +29,23 @@ void phase1_split_and_sort(const string& input_file, vector<string>& temp_files)
 
     while (infile.read(reinterpret_cast<char*>(buffer.data()), BLOQUE) || infile.gcount() > 0) {
         size_t items_read = infile.gcount() / sizeof(int);
-        sort(buffer.begin(), buffer.begin() + items_read);
+        buffer.resize(items_read);
+        sort(buffer.begin(), buffer.end());
 
         string temp_name = "temp_" + to_string(part++) + ".bin";
         ofstream temp_out(temp_name, ios::binary);
+        if (!temp_out) {
+            cerr << "Error al crear archivo temporal." << endl;
+            return;
+        }
         temp_out.write(reinterpret_cast<char*>(buffer.data()), items_read * sizeof(int));
-        temp_out.close();
+        if (!temp_out) {
+            cerr << "Error al escribir en archivo temporal." << endl;
+            return;
+        }
 
         temp_files.push_back(temp_name);
     }
-
-    infile.close();
 }
 
 void merge_two_files(const string& file1, const string& file2, const string& output_file) {
@@ -42,34 +53,50 @@ void merge_two_files(const string& file1, const string& file2, const string& out
     ifstream in2(file2, ios::binary);
     ofstream out(output_file, ios::binary);
 
+    if (!in1 || !in2 || !out) {
+        cerr << "Error al abrir archivos para fusionar." << endl;
+        return;
+    }
+
     vector<int> buffer1(B_LIMIT), buffer2(B_LIMIT), buffer_out(B_LIMIT);
     size_t idx1 = 0, idx2 = 0, out_idx = 0;
     size_t read1 = 0, read2 = 0;
 
-    auto refill_buffer = [](ifstream& in, vector<int>& buffer, size_t& count) {
-        in.read(reinterpret_cast<char*>(buffer.data()), BLOQUE);
-        count = in.gcount() / sizeof(int);
-    };
-
     refill_buffer(in1, buffer1, read1);
     refill_buffer(in2, buffer2, read2);
 
-    while (idx1 < read1 || idx2 < read2 || !in1.eof() || !in2.eof()) {
-        if (idx1 >= read1 && !in1.eof()) {
+    while (read1 > 0 || read2 > 0) {
+        while (idx1 < read1 && idx2 < read2) {
+            if (buffer1[idx1] <= buffer2[idx2]) {
+                buffer_out[out_idx++] = buffer1[idx1++];
+            } else {
+                buffer_out[out_idx++] = buffer2[idx2++];
+            }
+            if (out_idx == B_LIMIT) {
+                out.write(reinterpret_cast<char*>(buffer_out.data()), out_idx * sizeof(int));
+                out_idx = 0;
+            }
+        }
+
+        if (idx1 == read1 && in1) {
             refill_buffer(in1, buffer1, read1);
             idx1 = 0;
         }
-        if (idx2 >= read2 && !in2.eof()) {
+        if (idx2 == read2 && in2) {
             refill_buffer(in2, buffer2, read2);
             idx2 = 0;
         }
+    }
 
-        if (idx1 < read1 && (idx2 >= read2 || buffer1[idx1] <= buffer2[idx2])) {
-            buffer_out[out_idx++] = buffer1[idx1++];
-        } else if (idx2 < read2) {
-            buffer_out[out_idx++] = buffer2[idx2++];
+    while (idx1 < read1) {
+        buffer_out[out_idx++] = buffer1[idx1++];
+        if (out_idx == B_LIMIT) {
+            out.write(reinterpret_cast<char*>(buffer_out.data()), out_idx * sizeof(int));
+            out_idx = 0;
         }
-
+    }
+    while (idx2 < read2) {
+        buffer_out[out_idx++] = buffer2[idx2++];
         if (out_idx == B_LIMIT) {
             out.write(reinterpret_cast<char*>(buffer_out.data()), out_idx * sizeof(int));
             out_idx = 0;
@@ -79,50 +106,45 @@ void merge_two_files(const string& file1, const string& file2, const string& out
     if (out_idx > 0) {
         out.write(reinterpret_cast<char*>(buffer_out.data()), out_idx * sizeof(int));
     }
-
-    in1.close();
-    in2.close();
-    out.close();
 }
 
 
 
-void phase2_merge(const vector<string>& temp_files, const string& output_file, int arity = 0) { 
-    if (temp_files.size() == 1) {
-        remove(output_file.c_str());
-        rename(temp_files[0].c_str(), output_file.c_str());
-        return;
-    }
+void phase2_merge(vector<string> temp_files, const string& output_file, int arity = 0) {
+    while (temp_files.size() > 1) {
+        vector<string> next_round;
 
-    vector<string> temp_files2;
+        for (size_t i = 0; i < temp_files.size(); i += 2) {
+            if (i + 1 == temp_files.size()) {
+                next_round.push_back(temp_files[i]);
+            } else {
+                string merged_file = "merge_" + to_string(arity) + "_" + to_string(i / 2) + ".bin";
+                merge_two_files(temp_files[i], temp_files[i + 1], merged_file);
 
-    for (size_t i = 0; i < temp_files.size(); i += 2) {
-        string file1 = temp_files[i];
-        string file2 = (i + 1 < temp_files.size()) ? temp_files[i + 1] : "";
-        string temp_out_name = "merge_" + to_string(arity) + "_" + to_string(i / 2) + ".bin";
-
-        if (file2.empty()) {
-            // Si no hay segundo archivo, solo lo pasamos directo
-            temp_files2.push_back(file1);
-        } else {
-            merge_two_files(file1, file2, temp_out_name);
-            temp_files2.push_back(temp_out_name);
-
-            remove(file1.c_str());
-            remove(file2.c_str());
+                remove(temp_files[i]);
+                remove(temp_files[i + 1]);
+                next_round.push_back(merged_file);
+            }
         }
+        temp_files = move(next_round);
+        ++arity;
     }
 
-    phase2_merge(temp_files2, output_file, arity + 1);
+    if (!temp_files.empty()) {
+        remove(output_file);
+        rename(temp_files[0], output_file);
+    }
 }
 
 
 
-void sort_large_binary_file(const string& input_file, const string& output_file) {
+void sort_large_binary_file(const string& input_file, const string& output_file, size_t bloque = BLOQUE) {
     vector<string> temp_files;
     phase1_split_and_sort(input_file, temp_files);
-    phase2_merge(temp_files, output_file);
-    cout << "Ordenamiento completo." << endl;
+    if (!temp_files.empty()) {
+        phase2_merge(temp_files, output_file);
+        cout << "Ordenamiento completo." << endl;
+    }
 }
 
 int main() {
