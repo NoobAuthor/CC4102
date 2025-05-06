9ab5973150ff1f720524309a731f20c0f921bead
CincoVicenteCoronado <vicentemoriarti@gmail.com>
2025-05-06 15:39:00 -0400

    refactor: optimizar lectura de archivos
    - Se optimizó la forma en que se lee el archivo para aprovechar mejor las operaciones posibles en memoria principal, mejorando el rendimiento general.

--- T1/src/external_mergesort.cpp
+++ T1/src/external_mergesort.cpp
@@ -8,16 +8,32 @@
 #include <cstdio>
 #include <cstdint>  // para int64_t
 
-const size_t BLOQUE = 1024;
-const size_t B_LIMIT = BLOQUE / sizeof(int64_t);  // Cambié el tamaño del bloque a 64 bits
+const size_t BLOQUE = 4096
+const size_t M = 51200
+const size_t B_LIMIT = BLOQUE / sizeof(int64_t); 
+const size_t M_LIMIT = 51200/ sizeof(int64_t); 
 
 using namespace std;
 namespace fs = std::filesystem;
 
-void refill_buffer(ifstream& in, vector<int64_t>& buffer, size_t& count) {
-    in.read(reinterpret_cast<char*>(buffer.data()), BLOQUE);
-    count = in.gcount() / sizeof(int64_t);
+void refill_buffer(ifstream& in, vector<int64_t>& buffer, size_t size_buffer, size_t& count) {
+    count = 0;
+    size_t total_bytes_needed = size_buffer * sizeof(int64_t);
+
+    while (count * sizeof(int64_t) < total_bytes_needed && in) {
+        size_t bytes_to_read = min(BLOQUE, total_bytes_needed - count * sizeof(int64_t));
+        in.read(reinterpret_cast<char*>(buffer.data()) + count * sizeof(int64_t), bytes_to_read);
+        size_t bytes_read = in.gcount();
+
+        count += bytes_read / sizeof(int64_t);
+
+        if (bytes_read < bytes_to_read) {
+            // Si leímos menos de lo que pedimos, probablemente llegamos al final del archivo
+            break;
+        }
+    }
 }
+
 
 void phase1_split_and_sort(const string& input_file, vector<string>& temp_files) {
     ifstream infile(input_file, ios::binary);
@@ -26,14 +42,13 @@ void phase1_split_and_sort(const string& input_file, vector<string>& temp_files)
         return;
     }
 
-    vector<int64_t> buffer(B_LIMIT);
+    vector<int64_t> buffer(M_LIMIT);
     int part = 0;
+    size_t items_read = 0;
 
-    while (!infile.eof()) {
-        infile.read(reinterpret_cast<char*>(buffer.data()), BLOQUE);
-        size_t items_read = infile.gcount() / sizeof(int64_t);
-        if (items_read == 0) break;
+    refill_buffer(infile, buffer, M_LIMIT, items_read);
 
+    while (items_read > 0) {
         sort(buffer.begin(), buffer.begin() + items_read);
 
         string temp_name = "temp_" + to_string(part++) + ".bin";
@@ -49,8 +64,11 @@ void phase1_split_and_sort(const string& input_file, vector<string>& temp_files)
         }
 
         temp_files.push_back(temp_name);
+
+        refill_buffer(infile, buffer, M_LIMIT, items_read);
     }
 }
+
 
 void merge_two_files(const string& file1, const string& file2, const string& output_file) {
     ifstream in1(file1, ios::binary);
@@ -66,8 +84,8 @@ void merge_two_files(const string& file1, const string& file2, const string& out
     size_t idx1 = 0, idx2 = 0, out_idx = 0;
     size_t read1 = 0, read2 = 0;
 
-    refill_buffer(in1, buffer1, read1);
-    refill_buffer(in2, buffer2, read2);
+    refill_buffer(in1, buffer1, B_LIMIT, read1);
+    refill_buffer(in2, buffer2, B_LIMIT, read2);
 
     while (read1 > 0 || read2 > 0) {
         while (idx1 < read1 && idx2 < read2) {
@@ -83,11 +101,11 @@ void merge_two_files(const string& file1, const string& file2, const string& out
         }
 
         if (idx1 == read1 && in1) {
-            refill_buffer(in1, buffer1, read1);
+            refill_buffer(in1, buffer1, B_LIMIT, read1);
             idx1 = 0;
         }
         if (idx2 == read2 && in2) {
-            refill_buffer(in2, buffer2, read2);
+            refill_buffer(in2, buffer2, B_LIMIT, read2);
             idx2 = 0;
         }
     }
